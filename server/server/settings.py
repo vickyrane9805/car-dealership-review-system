dd645f64daa6da193bab443c92f93616dfc68017
vickyrane9805 <vivekrane9805@gmail.com>
2026-06-05 23:48:29 +0530

    Prepare for deployment

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-43bsr5d0v0%xpc2%ql@vw!fi+jc2+050=ycs!(tpx*#*eu#fjm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
